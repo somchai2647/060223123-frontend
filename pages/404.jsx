0540bea3a8f8044c02b388338139c1777ef4a462
somchai2647 <somchai.b@cvc.ac.th>
2022-10-21 00:09:00 +0700

    + 404Page

---
+++ pages/404.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Image from 'next/image'
+import Link from 'next/link';
+
+export default function Page404(props) {
+    return (
+        <Layout categorys={props.categorys}>
+            <div className="container text-center">
+                <Image
+                    src={"/assets/images/404-V2.svg"}
+                    width={400}
+                    height={400}
+                />
+                <h1>Error 404 Not Found!</h1>
+                <Link href={"/"}>
+                    <a className='btn btn-outline-primary'>กลับไปหน้าหลัก</a>
+                </Link>
+            </div>
+        </Layout>
+    )
+}
