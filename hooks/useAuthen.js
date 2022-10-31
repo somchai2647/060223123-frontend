7468181d15807d199056ca71d7e3728a159b09e5
somchai2647 <somchai.b@cvc.ac.th>
2022-10-31 15:37:34 +0700

    useAuthen

---
+++ hooks/useAuthen.js
@@ -0,0 +1,31 @@
+import { useContext } from "react"
+import AuthenContext from '../contexts/AuthenContext'
+import UserContext from '../contexts/UserContext'
+import Axios from '../components/Axios'
+import { useRouter } from 'next/router'
+import useSweetAlert from './useSweetAlert';
+
+export default function useAuthen() {
+    const authenContext = useContext(AuthenContext)
+    const userContext = useContext(UserContext)
+    const alert = useSweetAlert()
+    const router = useRouter()
+
+    async function logout() {
+        try {
+
+            if (data) {
+                authenContext.setIsLogin(false)
+                userContext.setUser({})
+                localStorage.removeItem("token")
+                router.push("/")
+                alert.toast("success", "ออกจากระบบสำเร็จ")
+            }
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    return { logout }
+}
