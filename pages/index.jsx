ca1fe824a2c97396f4c94d03b0fef63c3c6702e6
somchai2647 <somchai.b@cvc.ac.th>
2022-11-01 01:42:09 +0700

    + sharp & little optimic

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,32 @@
-import { useState, useEffect, useContext } from 'react'
+import { useState, useEffect, useContext, Suspense } from 'react'
+import dynamic from 'next/dynamic'
 import Axios from '../components/Axios'
 import Layout from '../components/Layout'
 import AuthenContext from '../contexts/AuthenContext'
 import UserContext from '../contexts/UserContext'
-import ProductList from '../components/Card/ProductList'
+import SVGLoading from '../components/SVGLoading'
+
+
+const ProductList = dynamic(() => import('../components/Card/ProductList'), {
+  suspense: true,
+  ssr: false
+})
 
 export default function Home(props) {
   const authenContext = useContext(AuthenContext);
   const userContext = useContext(UserContext);
   return (
     <Layout categorys={props.categorys}>
-      <ProductList keyword={""} title={"⭐ สินค้าแนะนำ"} isRecommend={true} />
+      <Suspense fallback={<SVGLoading />}>
+        <ProductList keyword={""} title={"⭐ สินค้าแนะนำ"} isRecommend={true} />
+        <ProductList keyword={""} title={"สินค้าใหม่"} />
+        <ProductList keyword={"SPY X FAMILY"} title={"SPY X FAMILY"} />
+        <ProductList keyword={"เกิดชาตินี้พี่ต้องเทพ"} title={"นิยายเรื่อง เกิดชาตินี้พี่ต้องเทพ"} />
+      </Suspense>
+      {/* <ProductList keyword={""} title={"⭐ สินค้าแนะนำ"} isRecommend={true} />
       <ProductList keyword={""} title={"สินค้าใหม่"} />
       <ProductList keyword={"SPY X FAMILY"} title={"SPY X FAMILY"} />
-      <ProductList keyword={"เกิดชาตินี้พี่ต้องเทพ"} title={"นิยายเรื่อง เกิดชาตินี้พี่ต้องเทพ"} />
+      <ProductList keyword={"เกิดชาตินี้พี่ต้องเทพ"} title={"นิยายเรื่อง เกิดชาตินี้พี่ต้องเทพ"} /> */}
     </Layout>
   )
 }
